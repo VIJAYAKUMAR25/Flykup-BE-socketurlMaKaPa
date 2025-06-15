3cc5b79cdfc329b90080f8949db3b1347523e996
VIJAYAKUMAR25 <vijay31797@gmail.com>
2025-06-15 19:47:25 +0530

    changes db

--- src/config/connection.js
+++ src/config/connection.js
@@ -5,10 +5,10 @@ const db_password = process.env.MONGO_PASSWORD;
 const db_cluster = process.env.MONGO_CLUSTER;
 
 // Development DataBase
-// const db_name = process.env.MONGO_DEV_DB;
+const db_name = process.env.MONGO_DEV_DB;
 
 // Production DataBase
-const db_name = process.env.MONGO_PROD_DB;
+// const db_name = process.env.MONGO_PROD_DB;
 
 
 const cloudURL = `mongodb+srv://${db_username}:${db_password}@${db_cluster}/${db_name}?retryWrites=true&w=majority&appName=Flykup`;
